67c74caa1b2101bf74f9280d4e4dbea84eb29fb8
sumrak10 <nurlan.urazkulov@gmail.com>
2023-11-04 16:38:56 +0500

    fix errror

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ADALIGHT_SERVER_HOST: str = '192.168.1.3'
-    ADALIGHT_SERVER_PORT: str = '3636'
+    ADALIGHT_SERVER_PORT: int = 3636
     LED_COUNT: int = 59
     SERIAL_PORT: str = '/dev/ttyUSB0'
     SERIAL_BAUDRATE: int = 115200
